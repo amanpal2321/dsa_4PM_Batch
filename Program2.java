5bc93608f10bd28c4c687df162d4d7cfa4d0b354
Aman Pal <amanpal2321@gmail.com>
2026-09-23 23:12:15 +0530

    Updated Code

--- Program2.java
+++ Program2.java
@@ -1,5 +1,17 @@
 public class Program2 {
     public static void main(String[] args) {
-        System.out.println("gckjn");
+        int[] arr ={1,15,6,3};
+        int numsum=0;
+        int digitsum=0;
+        for(int i=0;i<arr.length;i++){
+            numsum=numsum+arr[i];
+            int  num = arr[i];
+            while (num > 0) {
+                digitsum = digitsum + (num % 10);
+                num = num/10;
+                
+            }
+        }
+        System.out.println(numsum - digitsum);
     }
 }
